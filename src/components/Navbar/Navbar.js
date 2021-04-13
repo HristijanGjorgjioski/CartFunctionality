5b94213fac0ee99ec83461ae83e9db12e9371b23
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-04-13 18:53:48 +0200

    Working on navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AppBar, MenuItem, Toolbar, Box, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+
+import { MainContext } from '../../context/context';
+import useStyles from './styles';
 
 const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+    const { cartLength } = useContext(MainContext);
+    const classes = useStyles();
 
-export default Navbar
+    return (
+        <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar>
+                <Box display='flex' flexGrow={1}>
+                    <MenuItem><Typography className={classes.text}><Link className={classes.link} to="/">Food Ordering Portal</Link></Typography></MenuItem>
+                </Box>
+                <MenuItem><Link to="/cart" style={{ textDecoration: 'none' }}><ShoppingCart style={{ color: '#28334AFF' }} />{cartLength}</Link></MenuItem>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Navbar;
---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,16 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+    appBar: {
+        backgroundColor: '#FBDE44FF',
+        width: '100%',
+    },
+    text: {
+        color: 'black',
+        textDecoration: 'none'
+    },
+    link: {
+        color: 'black',
+        textDecoration: 'none'
+    }
+}))
